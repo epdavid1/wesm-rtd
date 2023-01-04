e7634647ed9a6be31463f784a5ba013347df5a0c
Edson <110080859+epdavid1@users.noreply.github.com>
2023-01-05 01:06:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,17 +11,24 @@ y_scaler = load(open('y_scaler.pkl', 'rb'))
 
 model = keras.models.load_model('model_358_12.h5')
 
-st.title('Electricity Market Price Forecasting')
-data = st.file_uploader("Upload excel file containing 12 rows of sequential historical supply, demand, and price values")
+st.title('WESM Load-Weighted Average Price Forecasting Tool')
+data = st.file_uploader("Upload CSV file containing supply, demand, and price values. The first 12 rows should contain historical values of supply, demand, and price. The succeeding rows should only contain projected values of supply and demand which will be used to forecast the price.")
 if data is not None:
     test = pd.read_csv(data)
     st.write(test)
-    fig = px.line(test['price'])
+    fig = px.line(test['price'][0:12])
     st.plotly_chart(fig)
     for i in range(0,12):
       test_arr = test[i:i+12].to_numpy()
       forecast = y_scaler.inverse_transform(model.predict(X_scaler.transform(test_arr).reshape(1,12,3)))
       test.at[i+12, "price"] = forecast
-    st.write(test)
-    fig = px.line(test['price'])
+    historical_price = test[['price']].copy()
+    historical_price.loc[12:24, 'price'] = np.nan
+    historical_price
+    forecasted_price = test[['price']].copy()
+    forecasted_price.loc[0:11, 'price'] = np.nan
+    price = pd.concat([historical_price, forecasted_price], axis=1)
+    price.columns = ['price', 'forecasted price']
+    st.write(price)
+    fig = px.line(price)
     st.plotly_chart(fig)
