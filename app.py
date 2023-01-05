d1b4e4f30d97f44e74089cff22bedea054f03270
Edson <110080859+epdavid1@users.noreply.github.com>
2023-01-05 14:18:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,28 +16,11 @@ model = keras.models.load_model('model_358_12.h5')
 st.title('WESM Load-Weighted Average Price Forecasting Tool')
 image = Image.open('example.png')
 st.image(image, caption='CSV format')
-test = pd.read_csv('test.csv')
-#st.write(test)
-#fig = px.line(test['price'][0:12])
-#st.plotly_chart(fig)
-for i in range(0,12):
-  test_arr = test[i:i+12].to_numpy()
-  forecast = y_scaler.inverse_transform(model.predict(X_scaler.transform(test_arr).reshape(1,12,3)))
-  test.at[i+12, "price"] = forecast
-historical_price = test[['price']].copy()
-historical_price.loc[12:24, 'price'] = np.nan
-forecasted_price = test[['price']].copy()
-forecasted_price.loc[0:11, 'price'] = np.nan
-price = pd.concat([historical_price, forecasted_price], axis=1)
-price.columns = ['price', 'forecasted price']
-price.index=list(range(0,120,5))
-#st.write(price)
-fig = px.line(price)
-fig.update_layout(
-    xaxis_title="Relative time in minutes",
-    yaxis_title="LWAP in PhP/MW"
-    )
-st.plotly_chart(fig)
+
+
+
+
+
 data = st.file_uploader("Upload CSV file containing supply, demand, and price values. The first 12 rows should contain historical values of supply, demand, and price. The succeeding rows should only contain projected values of supply and demand which will be used to forecast the price.")
 if data is not None:
     test = pd.read_csv(data)
@@ -62,3 +45,26 @@ if data is not None:
     yaxis_title="LWAP in PhP/MW"
     )
     st.plotly_chart(fig)
+else:
+  test = pd.read_csv('test.csv')
+  #st.write(test)
+  #fig = px.line(test['price'][0:12])
+  #st.plotly_chart(fig)
+  for i in range(0,12):
+    test_arr = test[i:i+12].to_numpy()
+    forecast = y_scaler.inverse_transform(model.predict(X_scaler.transform(test_arr).reshape(1,12,3)))
+    test.at[i+12, "price"] = forecast
+  historical_price = test[['price']].copy()
+  historical_price.loc[12:24, 'price'] = np.nan
+  forecasted_price = test[['price']].copy()
+  forecasted_price.loc[0:11, 'price'] = np.nan
+  price = pd.concat([historical_price, forecasted_price], axis=1)
+  price.columns = ['price', 'forecasted price']
+  price.index=list(range(0,120,5))
+  #st.write(price)
+  fig = px.line(price)
+  fig.update_layout(
+      xaxis_title="Relative time in minutes",
+      yaxis_title="LWAP in PhP/MW"
+      )
+  st.plotly_chart(fig)
